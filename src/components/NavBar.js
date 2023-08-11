96cd2336dcaecf94aac20cee4d2a8d37bf8fca3e
Morinke01 <ejulius250@gmail.com>
2023-08-11 08:19:20 +0300

    Assignment completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,66 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "inline-block",
+  padding: "12px 20px",
+  margin: "0 10px",
+  textDecoration: "none",
+  color: "white",
+  borderRadius: "4px",
+  background: "blue",
+  transition: "background 0.3s ease-in-out",
+  boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
+  fontWeight: "bold",
+};
+
+
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
